b4a2040246ac5f07883fa406f3518578c7988479
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-27 23:48:31 +0200

    Implemented fields Name, Level, Tag for Hero class

--- cs-401-project/Hero.cs
+++ cs-401-project/Hero.cs
@@ -2,5 +2,55 @@ namespace cs_401_project;
 
 public class Hero
 {
-    
+    public string Tag
+    {
+        get;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Tag cannot be empty!");
+            }
+
+            if (value.Length != 4)
+            {
+                throw new ArgumentException("Tag size should be exactly 4 characters!");
+            }
+
+            field = value;
+        }
+    }
+    public string Name
+    {
+        get;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Hero name cannot be empty!");
+            }
+
+            field = value;
+        }
+    }
+
+    public int Level
+    {
+        get;
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Hero level should be positive!");
+            }
+        }
+    }
+
+    public Hero(string name)
+    {
+        Name = name;
+        Level = 0;
+    }
+
+    public string GetNickname() => Name + "#" + Tag;
 }
